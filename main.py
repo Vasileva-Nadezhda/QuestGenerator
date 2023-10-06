56ed0b2e5a9c751b31fdd4ba4c55c96b7db42e76
Nodzomi <nv702822@gmail.com>
2023-10-06 14:12:31 +0300

    echo bot

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def picture(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('res/bhs_logo.png', 'rb'))
+
 
 if __name__ == '__main__':
-    print('PyCharm')
+    application = ApplicationBuilder().token('6236138189:AAG4Y4wAY23rWiO64B7UcDOMg_GB1oV2Bvk').build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
+    picture_handler = CommandHandler('picture', picture)
+    application.add_handler(picture_handler)
+    application.run_polling()
