f8322dfc8361c5794e95cdbb5777762508e4fef3
Nodzomi <nv702822@gmail.com>
2023-10-11 05:50:16 +0300

    models.py; mongoengine example

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 from dotenv import dotenv_values
 from pymongo import MongoClient
 
-config = dotenv_values(".env")
+import models
+import mongoengine
 
 
 def startup_db_client():
+    config = dotenv_values(".env")
     mongodb_client = MongoClient(config["ATLAS_URI"])
     print("Connected to the MongoDB database!")
     return mongodb_client
@@ -12,3 +14,6 @@ def startup_db_client():
 
 def shutdown_db_client(mongodb_client):
     mongodb_client.close()
+
+
+
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from sqlite3 import Date
 
-import database, models, mongoengine
 from dotenv import dotenv_values
+
+import database
+import models
+import mongoengine
 
 config = dotenv_values(".env")
 
 if __name__ == '__main__':
     client = database.startup_db_client()
     mongoengine.connect(db=config["DB_NAME"], host=config["ATLAS_URI"])
-    q1=models.Quest("A001", "A", "B", "C", Date(2023, 10, 10), "D", 10, 3)
-    q1.save()
     db = client[config["DB_NAME"]]
     quests = db['Quests']
     for i in quests.find():
